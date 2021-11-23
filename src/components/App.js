f77b7cf43702108e6fca86213ca5b6d0899769cc
Charles <chall70101@gmail.com>
2021-11-23 16:34:03 -0700

    fixed NODE_ENV variable

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const [pepperList, setPepperList]=useState([])
 
 let URL = 'http://localhost:3000'
 
-if(Node.env == 'production'){
+if(process.env.NODE_ENV == 'production'){
   URL = process.env.REACT_APP_API_URL
 }
 
